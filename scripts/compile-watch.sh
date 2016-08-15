bc9f11fcf856817e7e8168094b7f424bdb794b16
Jonathan Glanz <jonathan.glanz@gmail.com>
2016-08-15 00:51:55 -0400

    small tweaks

--- scripts/compile-watch.sh
+++ scripts/compile-watch.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-MUIOUT=${MUIOUT:?./build}
+#MUIOUT=${MUIOUT:?./build}
+if [ "${MUIOUT}" == "" ];then
+  MUIOUT=./build
+fi
 echo "Output Directory: ${MUIOUT}"
 
 babel ./src --ignore *.spec.js --watch --out-dir ${MUIOUT}
